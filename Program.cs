6cf7ff70598ec3094c85b2816a92a5f124258876
SteGriff <github@stegriff.co.uk>
2015-07-22 13:41:57 +0100

    init

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace guids
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int quantity = 1;
+            if (args.Length > 0 && !String.IsNullOrWhiteSpace(args[0]))
+            {
+                Int32.TryParse(args[0], out quantity);
+            }
+
+            for(int i = 0; i < quantity; i++)
+            {
+                Guid newGuid = Guid.NewGuid();
+                Console.WriteLine(newGuid);
+            }
+        }
+    }
+}
